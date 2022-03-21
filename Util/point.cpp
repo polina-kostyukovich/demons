e73d09ac387a0dec0476849541cf6e028d6b2b57
alexander <alexbiliba@gmail.com>
2022-03-21 15:49:50 +0300

    Add points

--- Util/point.cpp
+++ Util/point.cpp
@@ -1,1 +1,164 @@
 #include "point.h"
+
+Point::Point() {
+    x_ = 0;
+    y_ = 0;
+}
+Point::Point(long double x, long double y) {
+    x_ = x;
+    y_ = y;
+}
+Point::Point(const Point& point) {
+    x_ = point.x_;
+    y_ = point.y_;
+}
+Point::Point(Point&& point) {
+    x_ = point.x_;
+    y_ = point.y_;
+}
+
+bool Point::operator==(const Point& another_point) {
+    if (x_ != another_point.x_) {
+        return false;
+    }
+    return (y_ == another_point.y_);
+}
+bool Point::operator!=(const Point &another_point) {
+    return !(*this == another_point);
+}
+
+Point& Point::operator=(const Point& another_point) {
+    x_ = another_point.x_;
+    y_ = another_point.y_;
+    return *this;
+}
+Point& Point::operator=(Point&& another_point) {
+    if (another_point == *this) {
+        return *this;
+    }
+    x_ = another_point.x_;
+    y_ = another_point.y_;
+    return *this;
+}
+
+long double Point::Distance(const Point& first, const Point& second) {
+    long double delta_x = second.x_ - first.x_;
+    long double delta_y = second.y_ - first.y_;
+    long double squared_distance = (delta_x * delta_x) + (delta_y * delta_y);
+    return std::sqrt(squared_distance);
+}
+bool Point::IsClose(const Point& another_point) {
+    return (Point::Distance(*this, another_point) <= epsilon);
+}
+void Point::Normalize() {
+    long double sum = x_ + y_;
+    (*this) /= sum;
+}
+
+Point operator+(const Point& first, const Point& second) {
+    Point result(first);
+    result += second;
+    return result;
+}
+Point operator-(const Point& first, const Point& second) {
+    Point result(first);
+    result -= second;
+    return result;
+}
+Point operator*(const Point& first, const Point& second) {
+    Point result(first);
+    result *= second;
+    return result;
+}
+Point operator/(const Point& first, const Point& second) {
+    Point result(first);
+    result /= second;
+    return result;
+}
+Point& Point::operator+=(const Point& another_point) {
+    x_ += another_point.x_;
+    y_ += another_point.y_;
+    return *this;
+}
+Point& Point::operator-=(const Point& another_point) {
+    x_ -= another_point.x_;
+    y_ -= another_point.y_;
+    return *this;
+}
+Point& Point::operator*=(const Point& another_point) {
+    x_ *= another_point.x_;
+    y_ *= another_point.y_;
+    return *this;
+}
+Point& Point::operator/=(const Point& another_point) {
+    if (another_point.x_ == 0 || another_point.y_ == 0) {
+        throw "Bad division";
+        return *this;
+    }
+    x_ /= another_point.x_;
+    y_ /= another_point.y_;
+    return *this;
+}
+
+Point operator+(const Point& point, long double num) {
+    Point result(point);
+    result += num;
+    return result;
+}
+Point operator-(const Point& point, long double num) {
+    Point result(point);
+    result -= num;
+    return result;
+}
+Point operator*(const Point& point, long double num) {
+    Point result(point);
+    result *= num;
+    return result;
+}
+Point operator/(const Point& point, long double num) {
+    Point result(point);
+    result /= num;
+    return result;
+}
+Point operator+(long double num, const Point& point) {
+    return (point + num);
+}
+Point operator-(long double num, const Point& point) {
+    return (point - num);
+}
+Point operator*(long double num, const Point& point) {
+    return (point * num);
+}
+Point operator/(long double num, const Point& point) {
+    return (point / num);
+}
+Point& Point::operator+=(long double num) {
+    x_ += num;
+    y_ += num;
+    return *this;
+}
+Point& Point::operator-=(long double num) {
+    x_ -= num;
+    y_ -= num;
+    return *this;
+}
+Point& Point::operator*=(long double num) {
+    x_ *= num;
+    y_ *= num;
+    return *this;
+}
+Point& Point::operator/=(long double num) {
+    if (num == 0) {
+        throw "Bad division";
+        return *this;
+    }
+    x_ /= num;
+    y_ /= num;
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, const Point &point) {
+    out << point.x_ << " " << point.y_ << "\n";
+    return out;
+}
+
--- Util/point.h
+++ Util/point.h
@@ -1,7 +1,55 @@
-#ifndef UTIL_POINT_H_
-#define UTIL_POINT_H_
+#ifndef POINT_H
+#define POINT_H
+
+#include <complex>
+#include <ostream>
 
 class Point {
+public:
+    Point();
+    Point(long double x, long double y);
+    Point(const Point& point);
+    Point(Point&& point);
+
+    void Normalize();
+    bool IsClose(const Point& another_point);
+    static long double Distance(const Point& first, const Point& second);
+
+    bool operator==(const Point& another_point);
+    bool operator!=(const Point& another_point);
+
+    Point& operator=(const Point& another_point);
+    Point& operator=(Point&& another_point);
+
+    friend Point operator+(const Point& first, const Point& second);
+    friend Point operator-(const Point& first, const Point& second);
+    friend Point operator*(const Point& first, const Point& second);
+    friend Point operator/(const Point& first, const Point& second);
+    Point& operator+=(const Point& another_point);
+    Point& operator-=(const Point& another_point);
+    Point& operator*=(const Point& another_point);
+    Point& operator/=(const Point& another_point);
+
+    friend Point operator+(const Point& point, long double num);
+    friend Point operator-(const Point& point, long double num);
+    friend Point operator*(const Point& point, long double num);
+    friend Point operator/(const Point& point, long double num);
+    friend Point operator+(long double num, const Point& point);
+    friend Point operator-(long double num, const Point& point);
+    friend Point operator*(long double num, const Point& point);
+    friend Point operator/(long double num, const Point& point);
+    Point& operator+=(long double num);
+    Point& operator-=(long double num);
+    Point& operator*=(long double num);
+    Point& operator/=(long double num);
+
+    friend std::ostream& operator<<(std::ostream& out, const Point& point);
+
+    constexpr static const long double epsilon = 10;
+private:
+    long double x_;
+    long double y_;
 };
 
-#endif  // UTIL_POINT_H_
+
+#endif  // POINT_H
