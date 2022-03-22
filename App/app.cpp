35fc59568933289c9f8ba6cdfe0b4c8dc6b57fb4
Lomcka <rudovichdmitry@gmail.com>
2022-03-22 10:12:39 +0300

    Add app

---
+++ App/app.cpp
@@ -0,0 +1,17 @@
+#include <cassert>
+#include "app.h"
+
+App::App(const Model* model, const View* view, const Controller* controller) {
+  if (is_running_) {
+    assert(false);
+  }
+
+  model_ = std::make_unique<Model>(*model);
+  view_ = std::make_unique<View>(*view);
+  controller_ = std::make_unique<Controller>(*controller);
+  is_running_ = true;
+}
+
+void App::Run() {
+
+}
---
+++ App/app.h
@@ -0,0 +1,19 @@
+#ifndef APP_APP_H
+#define APP_APP_H
+
+#include <memory>
+
+class App {
+ public:
+  App(const Model* model, const View* view, const Controller* controller);
+
+  void Run();
+
+ private:
+  std::unique_ptr<Model> model_ = nullptr;
+  std::unique_ptr<View> view_ = nullptr;
+  std::unique_ptr<Controller> controller_ = nullptr;
+  inline static bool is_running_ = false;
+};
+
+#endif  // APP_APP_H
