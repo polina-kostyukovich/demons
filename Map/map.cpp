4480a66ba23f045da6856de03b5ce8a6430af330
Polina <polyushka.kostyukovich@gmail.com>
2022-03-24 22:57:18 +0300

    Change raw pointers to unique_ptrs

--- Map/map.cpp
+++ Map/map.cpp
@@ -3,14 +3,16 @@
 Map::Map(int window_width, int window_height) {
   columns_ = (window_width - 1) / constants::kObjectSize + 1;
   rows_ = (window_height - 1) / constants::kObjectSize + 1;
-  objects_ = std::vector<std::vector<GameObject*>>(
-      columns_, std::vector<GameObject*>(rows_, nullptr));
+  objects_.resize(columns_);
+  for (auto &column : objects_) {
+    column.resize(rows_);
+  }
 }
 
-void Map::SetObject(GameObject* object, int x, int y) {
-  objects_[x][y] = object;
+void Map::SetObject(std::unique_ptr<GameObject>&& object, int x, int y) {
+  objects_[x][y] = std::move(object);
 }
 
-const GameObject* Map::GetObject(int x, int y) const {
+const std::unique_ptr<GameObject>& Map::GetObject(int x, int y) const {
   return objects_[x][y];
 }
--- Map/map.h
+++ Map/map.h
@@ -1,6 +1,7 @@
 #ifndef MAP_MAP_H_
 #define MAP_MAP_H_
 
+#include <memory>
 #include <vector>
 #include "../GameObject/game_object.h"
 #include "../Model/constants.h"
@@ -8,13 +9,13 @@
 class Map {
  public:
   Map(int window_width, int window_height);
-  void SetObject(GameObject* object, int x, int y);
-  const GameObject* GetObject(int x, int y) const;
+  void SetObject(std::unique_ptr<GameObject>&& object, int x, int y);
+  const std::unique_ptr<GameObject>& GetObject(int x, int y) const;
 
  private:
   int columns_;
   int rows_;
-  std::vector<std::vector<GameObject*>> objects_;
+  std::vector<std::vector<std::unique_ptr<GameObject>>> objects_;
 };
 
 #endif  // MAP_MAP_H_
