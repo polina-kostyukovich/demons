e59196fd0925a2b77fdd67a265ad2443884dbe43
Polina <polyushka.kostyukovich@gmail.com>
2022-03-24 19:25:23 +0300

    Add map

--- Map/map.cpp
+++ Map/map.cpp
@@ -1,1 +1,16 @@
 #include "map.h"
+
+Map::Map(int window_width, int window_height) {
+  columns_ = (window_width - 1) / constants::kObjectSize + 1;
+  rows_ = (window_height - 1) / constants::kObjectSize + 1;
+  objects_ = std::vector<std::vector<GameObject*>>(
+      columns_, std::vector<GameObject*>(rows_, nullptr));
+}
+
+void Map::SetObject(GameObject* object, int x, int y) {
+  objects_[x][y] = object;
+}
+
+const GameObject* Map::GetObject(int x, int y) const {
+  return objects_[x][y];
+}
--- Map/map.h
+++ Map/map.h
@@ -1,7 +1,20 @@
 #ifndef MAP_MAP_H_
 #define MAP_MAP_H_
 
+#include <vector>
+#include "../GameObject/game_object.h"
+#include "../Model/constants.h"
+
 class Map {
+ public:
+  Map(int window_width, int window_height);
+  void SetObject(GameObject* object, int x, int y);
+  const GameObject* GetObject(int x, int y) const;
+
+ private:
+  int columns_;
+  int rows_;
+  std::vector<std::vector<GameObject*>> objects_;
 };
 
 #endif  // MAP_MAP_H_
