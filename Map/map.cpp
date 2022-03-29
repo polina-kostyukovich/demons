2523769d3face5c65c0dbe341439b8ed31064818
polina-kostyukovich <99528525+polina-kostyukovich@users.noreply.github.com>
2022-03-29 15:29:23 +0300

    Map (#5)
    * base commit
    
    * Add map
    
    * Change raw pointers to unique_ptrs
    
    * Add <utility> to map.cpp
    
    * Add asserts and RemoveObject()
    
    * Correct inaccuracies
    
    * Delete unnecessary constant

--- Map/map.cpp
+++ Map/map.cpp
@@ -1,1 +1,27 @@
 #include "map.h"
+
+#include <cassert>
+#include <utility>
+
+Map::Map(int window_width, int window_height) {
+  // todo write json
+  objects_.resize(columns_);
+  for (auto& column : objects_) {
+    column.resize(rows_);
+  }
+}
+
+void Map::SetObject(std::unique_ptr<GameObject>&& object, int x, int y) {
+  assert(x >= 0 && x < columns_ && y >= 0 && y < rows_);
+  objects_[x][y] = std::move(object);
+}
+
+const std::unique_ptr<GameObject>& Map::GetObject(int x, int y) const {
+  assert(x >= 0 && x < columns_ && y >= 0 && y < rows_);
+  return objects_[x][y];
+}
+
+void Map::RemoveObject(int x, int y) {
+  assert(x >= 0 && x < columns_ && y >= 0 && y < rows_);
+  objects_[x][y] = nullptr;
+}
--- Map/map.h
+++ Map/map.h
@@ -1,7 +1,23 @@
 #ifndef MAP_MAP_H_
 #define MAP_MAP_H_
 
+#include <memory>
+#include <vector>
+
+#include "../GameObject/game_object.h"
+#include "../Model/constants.h"
+
 class Map {
+ public:
+  Map(int window_width, int window_height);
+  void SetObject(std::unique_ptr<GameObject>&& object, int x, int y);
+  const std::unique_ptr<GameObject>& GetObject(int x, int y) const;
+  void RemoveObject(int x, int y);
+
+ private:
+  int columns_;
+  int rows_;
+  std::vector<std::vector<std::unique_ptr<GameObject>>> objects_;
 };
 
 #endif  // MAP_MAP_H_
