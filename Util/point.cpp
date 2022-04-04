12f8ca273d9a1b4753373289dc6fa20b001783e7
Polina <polyushka.kostyukovich@gmail.com>
2022-04-04 22:32:08 +0300

    Add some helpful methods to Point

--- Util/point.cpp
+++ Util/point.cpp
@@ -17,19 +17,21 @@ Point::Point(Point&& point) {
     y_ = point.y_;
 }
 
+bool Point::IsClose(const Point& another_point) const {
+  return (Distance(*this, another_point) <= constants::kClose);
+}
+
 long double Point::Distance(const Point& first, const Point& second) {
     long double delta_x = second.x_ - first.x_;
     long double delta_y = second.y_ - first.y_;
     long double squared_distance = (delta_x * delta_x) + (delta_y * delta_y);
     return std::sqrt(squared_distance);
 }
-bool Point::IsClose(const Point& another_point) const {
-    return (Distance(*this, another_point) <= constants::kClose);
-}
 
 void Point::SetX(long double new_x) {
     x_ = new_x;
 }
+
 void Point::SetY(long double new_y) {
     y_ = new_y;
 }
@@ -37,6 +39,7 @@ void Point::SetY(long double new_y) {
 long double Point::GetX() const {
     return x_;
 }
+
 long double Point::GetY() const {
     return y_;
 }
@@ -44,6 +47,7 @@ long double Point::GetY() const {
 bool Point::operator==(const Point& another_point) const {
     return (Distance(*this, another_point) <= constants::kEpsilon);
 }
+
 bool Point::operator!=(const Point& another_point) const {
     return !(*this == another_point);
 }
@@ -53,10 +57,50 @@ Point& Point::operator=(const Point& another_point) {
     y_ = another_point.y_;
     return *this;
 }
+
 Point& Point::operator=(Point&& another_point) {
     x_ = another_point.x_;
     y_ = another_point.y_;
     return *this;
+}
+
+Point& Point::operator+=(const Point& another_point) {
+  x_ += another_point.x_;
+  y_ += another_point.y_;
+  return *this;
+}
+Point& Point::operator-=(const Point& another_point) {
+  x_ -= another_point.x_;
+  y_ -= another_point.y_;
+  return *this;
+}
+
+Point& Point::operator+=(const Vector2D& vector) {
+  x_ += vector.GetX();
+  y_ += vector.GetY();
+  return *this;
+}
+
+Point& Point::operator-=(const Vector2D& vector) {
+  x_ -= vector.GetX();
+  y_ -= vector.GetY();
+  return *this;
+}
+
+Point& Point::operator*=(long double num) {
+  x_ *= num;
+  y_ *= num;
+  return *this;
+}
+
+Point& Point::operator/=(long double num) {
+  if (num == 0) {
+    std::cerr << "Zero division\n";
+    assert(false);
+  }
+  x_ /= num;
+  y_ /= num;
+  return *this;
 }
 
 Point operator+(const Point& first, const Point& second) {
@@ -64,20 +108,27 @@ Point operator+(const Point& first, const Point& second) {
     result += second;
     return result;
 }
+
 Point operator-(const Point& first, const Point& second) {
     Point result(first);
     result -= second;
     return result;
 }
-Point& Point::operator+=(const Point& another_point) {
-    x_ += another_point.x_;
-    y_ += another_point.y_;
-    return *this;
+
+Point operator+(const Point& point, const Vector2D& vector) {
+  Point result(point);
+  result += vector;
+  return result;
 }
-Point& Point::operator-=(const Point& another_point) {
-    x_ -= another_point.x_;
-    y_ -= another_point.y_;
-    return *this;
+
+Point operator+(const Vector2D& vector, const Point& point) {
+  return point + vector;
+}
+
+Point operator-(const Point& point, const Vector2D& vector) {
+  Point result(point);
+  result -= vector;
+  return result;
 }
 
 Point operator*(const Point& point, long double num) {
@@ -85,27 +136,15 @@ Point operator*(const Point& point, long double num) {
     result *= num;
     return result;
 }
+
 Point operator/(const Point& point, long double num) {
     Point result(point);
     result /= num;
     return result;
 }
+
 Point operator*(long double num, const Point& point) {
     return (point * num);
-}
-Point& Point::operator*=(long double num) {
-    x_ *= num;
-    y_ *= num;
-    return *this;
-}
-Point& Point::operator/=(long double num) {
-    if (num == 0) {
-        std::cerr << "Zero division\n";
-        assert(false);
-    }
-    x_ /= num;
-    y_ /= num;
-    return *this;
 }
 
 std::ostream& operator<<(std::ostream& out, const Point& point) {
