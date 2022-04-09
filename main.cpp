251a7bb5c5ed4f84b8b8ca557c177994d1a53f42
alexbiliba <alexbiliba@gmail.com>
2022-04-09 20:38:02 +0300

    main.cpp fixed update: created App for first window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <QApplication>
 
-#include "Controller/controller.h"
+#include "App/app.h"
 
 int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
-  Controller controller;
+
+  std::unique_ptr<Model> model(new Model);
+  std::unique_ptr<View> view(new View);
+  std::unique_ptr<Controller> controller(new Controller);
+
+  App m_app(std::move(model),
+            std::move(view),
+            std::move(controller));
+
   return QApplication::exec();
 }
