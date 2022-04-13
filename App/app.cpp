47cd2ff1843bf48af6b2a0ea8c7905a8524929e1
alexbiliba <alexbiliba@gmail.com>
2022-04-13 23:40:15 +0300

    View update fix: functions order

--- App/app.cpp
+++ App/app.cpp
@@ -2,12 +2,12 @@
 
 #include <utility>
 
-void App::Run() {
-  view_->show();
-}
-
 App::App() {
   std::unique_ptr<Model> model(new Model);
   std::unique_ptr<Controller> controller(new Controller(std::move(model)));
   view_ = std::make_unique<View>(std::move(controller));
+}
+
+void App::Run() {
+  view_->show();
 }
