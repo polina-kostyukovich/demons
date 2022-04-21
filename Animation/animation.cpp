a7e5901166c2c1aa102ef2f96d7b2208dec5bb08
Irina <irishavoronkina@gmail.com>
2022-04-21 22:43:57 +0300

    Really last

--- Animation/animation.cpp
+++ Animation/animation.cpp
@@ -14,28 +14,28 @@ Animation::Animation() {
 
 void Animation::MakeHero() {
   std::string str = ":Picture/Hero/demonessa";
-  for (int i=0; i<=6; i++) {
-    str+=std::to_string(i);
-    str+=".png";
+  for (int i = 0; i <= 6; i++) {
+    str += std::to_string(i);
+    str += ".png";
     hero_pixmaps.emplace_back(QPixmap(str.c_str()));
   }
-  for (int i=5; i>=0; i--) {
-    str+=std::to_string(i);
-    str+=".png";
+  for (int i = 5; i >= 0; i--) {
+    str += std::to_string(i);
+    str += ".png";
     hero_pixmaps.emplace_back(QPixmap(str.c_str()));
   }
 }
 
 void Animation::MakeWings() {
   std::string str = ":Picture/Wings/wings";
-  for (int i=0; i<=6; i++) {
-    str+=std::to_string(i);
-    str+=".png";
+  for (int i = 0; i <= 6; i++) {
+    str += std::to_string(i);
+    str += ".png";
     hero_pixmaps.emplace_back(QPixmap(str.c_str()));
   }
-  for (int i=5; i>=0; i--) {
-    str+=std::to_string(i);
-    str+=".png";
+  for (int i = 5; i >= 0; i--) {
+    str += std::to_string(i);
+    str += ".png";
     hero_pixmaps.emplace_back(QPixmap(str.c_str()));
   }
 }
