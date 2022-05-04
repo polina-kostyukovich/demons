9b8d5a41e13734763cdea572f16900b71be65655
Lomcka <rudovichdmitry@gmail.com>
2022-05-04 14:58:25 +0300

    Fireball attack: fixed compilation error

--- View/view.cpp
+++ View/view.cpp
@@ -54,7 +54,7 @@ void View::keyReleaseEvent(QKeyEvent* event) {
 void View::mousePressEvent(QMouseEvent *event) {
   Point mouse_pos = {static_cast<long double>(cursor().pos().x()),
                      static_cast<long double>(cursor().pos().y())};
-  controller_->HandleMousePressEvent(event, mouse_pos);
+  controller_->HandleMousePressEvent(event);
 }
 
 void View::mouseReleaseEvent(QMouseEvent *event) {
